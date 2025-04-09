ebcd0c667224dbcc13d53dddfbb7eb4ca7ff314a
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-04-09 08:14:44 +0700

    Menambahkan Class DataMahasiswa103022300034

---
+++ modul7_kelompok7/DataMahasiswa_103022300034.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok7
+{
+    internal class DataMahasiswa103022300034
+    {
+        public async Task<Mahasiswa> ReadJsonAsync(string filePath)
+        {
+            try
+            {
+                using FileStream openStream = File.OpenRead(filePath);
+                var mahasiswa = await JsonSerializer.DeserializeAsync<Mahasiswa>(openStream);
+
+                if (mahasiswa != null)
+                {
+                    Console.WriteLine($"Nama {mahasiswa.FirstName} {mahasiswa.LastName} dengan nim {mahasiswa.Nim} " +
+                        $"dari fakultas {mahasiswa.Fakultas} dengan gender {mahasiswa.Gender} berumur {mahasiswa.Age}" +
+                        $" ber alamat di {mahasiswa.Address}");
+                }
+
+                return mahasiswa!;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the JSON file: {ex.Message}");
+                return null!;
+            }
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public long Nim { get; set; }
+        public required string Fakultas { get; set; }
+        public required string Age { get; set; }
+        public required string Gender { get; set; }
+        public required string Address { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+
+    public class Course
+    {
+        public required string name { get; set; }
+        public int code { get; set; }
+    }
+}
--- modul7_kelompok7/Program.cs
+++ modul7_kelompok7/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok7;
+using System;
+using System.Threading.Tasks;
+using modul7_kelompok7;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var dataMahasiswa = new DataMahasiswa103022300034();
+        var mahasiswaFilePath = @"jurnal7_1_103022300034.json";
+        await dataMahasiswa.ReadJsonAsync(mahasiswaFilePath);
+
+        
+    }
+}
